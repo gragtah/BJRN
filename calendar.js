e7fd2e27feb96ad821fce0d201f751fe07454e18
Pratyush Nalam <pratyushnalam@outlook.com>
2015-12-23 01:20:06 -0500

    adding free busy to calendar (sj)

--- calendar.js
+++ calendar.js
@@ -35,19 +35,51 @@ gCal.getFreeTimes = function(callback) {
     today = today.toISOString();
     var nextWeek = moment().add(7, 'd');
     nextWeek = nextWeek.toISOString();
-    var resource = {
+    calendar.events.list({
+      calendarId: googleConfig.calendarId,
+      maxResuts: 20,
       timeMin: today,
       timeMax: nextWeek,
-      items:[
-      {
-        id: calendarId
-      }
-      ]
-    };
-    var response = Calendar.Freebusy.query(resource);
-    return response.calendars[calendarId];
+      auth: oAuthClient
+    }, function (err, events) {
+      if(err) {
+        console.log("error fetching events");
+        console.log(err);
+      } else {
+        console.log("Successfully fetched events");
+        console.log(events);
 
+        for(var i = 0; i < events.items.length; i++) {
+          console.log([events.items[i].start.dateTime, events.items[i].end.dateTime]);
+        };
+
+        for(var i = today.slice(8, 10); i <= nextWeek.slice(8, 10); i++) {
+          console.log("Day " + i);
+          var x = findSlotsAvailable(events, i);
+          if(x == false) {
+            console.log("Full day available");
+          }
+        }
+      }
+    });
   }
+}
+
+function findSlotsAvailable(events, currentDay) {
+  var flag = false;
+  for(var i = 0; i < events.items.length; i++) {
+    if(events.items[i].start.dateTime.slice(8, 10) == currentDay) {
+      flag = true;
+      for(var j = 6; j < 24; j++) {
+        if((j + 3 <= events.items[i].start.dateTime.slice(11, 13)) || (j >= events.items[i].end.dateTime.slice(11, 13))) {
+          console.log(j + "-" + (j + 3) + " available");
+        } else {
+          console.log(j + "-" + (j + 3) + " not available");
+        }
+      }
+    }
+  };
+  return flag;
 }
 
 // Return point for oAuth flow, should match googleConfig.redirectURL
@@ -76,10 +108,3 @@ gCal.auth = function(callback){
     } 
 };
 
-
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Listening at http://%s:%s', host, port);
-});
