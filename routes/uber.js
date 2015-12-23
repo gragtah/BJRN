e326e9ff57726dddeb9c4d66fce9c18516a441be
Saurav <jain.saurav@hotmail.com>
2015-12-23 10:51:28 -0500

    sqs

--- routes/uber.js
+++ routes/uber.js
@@ -4,7 +4,14 @@ var uber = require('../uber');
 var authUrl = "https://login.uber.com/oauth/authorize";
 var request=require('request');
 var requrl='https://sandbox-api.uber.com/v1/sandbox/requests/'
-var uberClient=require('../uber')
+var uberClient = new uber({
+  client_id: 'Gr-QobTHsCgp41tES3-cyPnfo-KvI1Yf',
+  client_secret: 'N9N1gjS1oohlg700KYPsiStSHSfRUX0GWYLbBRRP',
+  server_token: 'TRKbz3z1bPk1NQ77-sY_4W_xXljyGE3ypnJ727K_',
+  redirect_uri: 'http://localhost:3000/uber',
+  name: 'cloud9'
+});
+
 router.get('/', function(req, res, next) {
   res.send('this is uber api');
 });
